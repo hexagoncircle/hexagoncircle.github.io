93627df26eb8d3bca41f386051ed2fff1c69181a
Ryan Mulligan <rmulligan@inspirato.com>
2020-12-13 18:01:38 -0800

    Clean up code

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 const slider = document.querySelector(".theme-slider");
 
-const setThemeSettings = () => {
+const setThemeValue = (value) => {
+  document.body.dataset.theme = value;
+  slider.value = value;
+};
+
+const getThemeValue = () => {
   if (localStorage.theme) {
-    document.body.dataset.theme = localStorage.theme;
-    slider.value = localStorage.theme;
+    setThemeValue(localStorage.theme);
     return;
   }
 
   if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-    document.body.dataset.theme = 5;
-    slider.value = 5;
+    setThemeValue(5);
   }
 };
 
-setThemeSettings();
+getThemeValue();
 setTimeout(() => document.body.style.setProperty("--duration", "200ms"), 1000);
 
-[...images].forEach((image) => {
-  image.addEventListener("load", (event) => {
-    event.target.removeAttribute("data-is-loading");
-  });
-});
-
 slider.addEventListener("input", () => {
-  document.body.dataset.theme = slider.value;
+  setThemeValue(slider.value);
   localStorage.setItem("theme", slider.value);
 });
