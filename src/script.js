aa119ae1b235ec76563341222d61e2bba5b191a5
Ryan Mulligan <rmulligan@inspirato.com>
2020-12-14 11:41:50 -0800

    Refactor lazy loading implementation

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+const images = document.querySelectorAll("img");
 const slider = document.querySelector(".theme-slider");
 
 const setThemeValue = (value) => {
@@ -10,14 +11,21 @@ const getThemeValue = () => {
     setThemeValue(localStorage.theme);
     return;
   }
-
   if (window.matchMedia("(prefers-color-scheme: light)").matches) {
     setThemeValue(5);
   }
 };
 
+document.body.removeAttribute("data-no-js");
+
 getThemeValue();
 setTimeout(() => document.body.style.setProperty("--duration", "200ms"), 1000);
+
+[...images].forEach((image) => {
+  image.addEventListener("load", (event) => {
+    event.target.removeAttribute("data-is-loading");
+  });
+});
 
 slider.addEventListener("input", () => {
   setThemeValue(slider.value);
