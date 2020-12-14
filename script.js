9b19c0ded4466d1d31176da65facf19f273648d0
Ryan Mulligan <rmulligan@inspirato.com>
2020-12-13 17:23:40 -0800

    Refactor transitioning

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 const images = document.querySelectorAll("img");
 const slider = document.querySelector(".theme-slider");
 
+const setThemeSettings = () => {
+  if (localStorage.theme) {
+    document.body.dataset.theme = localStorage.theme;
+    slider.value = localStorage.theme;
+    return;
+  }
+
+  if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+    document.body.dataset.theme = 5;
+    slider.value = 5;
+  }
+};
+
+setThemeSettings();
+setTimeout(() => document.body.style.setProperty("--duration", "200ms"), 1000);
+
 [...images].forEach((image) => {
   image.addEventListener("load", (event) => {
     event.target.removeAttribute("data-is-loading");
   });
 });
-
-if (
-  !localStorage.theme &&
-  window.matchMedia("(prefers-color-scheme: light)").matches
-) {
-  document.body.dataset.theme = 5;
-  slider.value = 5;
-}
-
-if (localStorage.theme) {
-  document.body.dataset.theme = localStorage.theme;
-  slider.value = localStorage.theme;
-}
-
-window.addEventListener("load", () =>
-  document.body.classList.remove("preload")
-);
 
 slider.addEventListener("input", () => {
   document.body.dataset.theme = slider.value;
